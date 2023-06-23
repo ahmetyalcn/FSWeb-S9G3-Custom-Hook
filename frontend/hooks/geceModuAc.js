944b35868084e61613655b34a399056d99bfe05e
Ahmet Necdet Yalçın <ahmetnecdetyalcin@Ahmet-MacBook-Air.local>
2023-06-23 20:11:22 +0300

    dosya güncelledni

---
+++ frontend/hooks/geceModuAc.js
@@ -0,0 +1,6 @@
+import { useLocalStorage } from "../localStorageKullan";
+
+export const geceModuAc = (initialValue) => {
+    const [geceModu, setGeceModu] = useLocalStorage("geceModu" ,initialValue);
+    return [geceModu, setGeceModu];
+  };
